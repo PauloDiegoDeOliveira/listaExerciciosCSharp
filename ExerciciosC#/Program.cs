6f11508f97cf2fd8d45c5698ebb4bff440bc4e64
Paulo Diego <paulo.diego@unip.br>
2023-03-04 10:27:46 -0300

    Sorteio

---
+++ ExerciciosC#/Program.cs
@@ -0,0 +1,5 @@
+﻿// Sorteio
+string[] nomes = { "Gustavo C", "Gustavo A", "José", "Diego", "Fickert", "Tulio", "Paulo" };
+Random aleatorio = new();
+string sorteio = nomes[aleatorio.Next(nomes.Length)];
+Console.WriteLine("O nome sorteado é: " + sorteio);
